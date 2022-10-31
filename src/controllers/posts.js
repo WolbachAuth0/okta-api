1a33a1057a58d3c5ba25e63ecd2267c851e8d92f
WolbachAuth0 <aaron.wolbach@okta.com>
2022-10-31 09:50:34 -0400

    remove fetch from local data

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,15 +1,12 @@
 const axios = require('axios')
 const { logger } = require('express-winston')
 const responseFormatter = require('../middleware/responseFormatter')
-const items = require('./data')
 
 // https://jsonplaceholder.typicode.com/
 
 module.exports = {
   listAll,
-  listAllLocal,
   getByID,
-  getByIDLocal
 }
 
 function handleError(req, res, error) {
@@ -28,22 +25,8 @@ async function listAll (req, res) {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
     const options = {
       status: 200,
-      message: `found ${response.data.length} items from remote store`,
+      message: `found ${response.data.length} posts from remote store`,
       data: response.data
-    }
-    const json = responseFormatter(req, res, options)
-    res.status(options.status).json(json)
-  } catch (error) {
-    return handleError(error)
-  }
-}
-
-async function listAllLocal (req, res) {
-  try {
-    const options = {
-      status: 200,
-      message: `found ${items.length} items from local store`,
-      data: items
     }
     const json = responseFormatter(req, res, options)
     res.status(options.status).json(json)
@@ -62,24 +45,6 @@ async function getByID (req, res) {
       status: response.data ? 200: 404,
       message: response.data ? `found item with id: ${id} in remote store` : `item with id: ${id} not found in remote store`,
       data: response.data
-    }
-    const json = responseFormatter(req, res, options)
-    res.status(options.status).json(json)
-  } catch (error) {
-    return handleError(error)
-  }
-}
-
-async function getByIDLocal (req, res) {
-  try {
-    const id = req.params.post_id
-    
-    const item = items.find(x => x.id == id)
-    
-    const options = {
-      status: item ? 200: 404,
-      message: item ? `found item with id: ${id} in local store` : `item with id: ${id} not found in local store`,
-      data: item ? item : {}
     }
     const json = responseFormatter(req, res, options)
     res.status(options.status).json(json)
